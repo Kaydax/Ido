06442d98984c901b7651d075ee821b2a9ad441d8
Bek Madiyar <74451653+Bekos-von-Nimmerland@users.noreply.github.com>
2020-11-16 09:44:13 +0600

    Removing ModKeys initiation from ClientProxy.java
    Removing ModKeys initiation from ClientProxy.java

--- src/main/java/xyz/kaydax/ido/proxy/ClientProxy.java
+++ src/main/java/xyz/kaydax/ido/proxy/ClientProxy.java
@@ -3,15 +3,12 @@ package xyz.kaydax.ido.proxy;
 import net.minecraftforge.common.MinecraftForge;
 import xyz.kaydax.ido.handler.ClientHandler;
 import xyz.kaydax.ido.handler.CommonHandler;
-import xyz.kaydax.ido.init.ModKeys;
 
 public class ClientProxy extends CommonProxy
 {
   @Override
   public void init()
-  {
-	ModKeys.init();
-	ModKeys.register();	  
+  {  
     MinecraftForge.EVENT_BUS.register(new CommonHandler());
     MinecraftForge.EVENT_BUS.register(new ClientHandler());
   }
